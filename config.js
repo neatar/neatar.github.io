c1affa74118b03fd44ec00c4fcc025350088d544
Ilyar <761285+ilyar@users.noreply.github.com>
2021-09-19 08:23:58 +0300

    Updates

---
+++ config.js
@@ -0,0 +1,43 @@
+export function config(env) {
+  switch (env) {
+    case 'production':
+    case 'mainnet':
+      return {
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        contractName: 'alpha.neatar.near',
+        walletUrl: 'https://wallet.near.org',
+        helperUrl: 'https://helper.mainnet.near.org',
+        explorerUrl: 'https://explorer.mainnet.near.org',
+      }
+    case 'development':
+    case 'testnet':
+      return {
+        networkId: 'testnet',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        contractName: 'dev-1630244685532-93937831175122',
+        walletUrl: 'https://wallet.testnet.near.org',
+        helperUrl: 'https://helper.testnet.near.org',
+        explorerUrl: 'https://explorer.testnet.near.org',
+      }
+    case 'betanet':
+      return {
+        networkId: 'betanet',
+        nodeUrl: 'https://rpc.betanet.near.org',
+        contractName: 'alpha.neatar.betanet',
+        walletUrl: 'https://wallet.betanet.near.org',
+        helperUrl: 'https://helper.betanet.near.org',
+        explorerUrl: 'https://explorer.betanet.near.org',
+      }
+    case 'local':
+      return {
+        networkId: 'local',
+        nodeUrl: 'http://localhost:3030',
+        contractName: 'alpha.neatar.local',
+        walletUrl: 'http://localhost:4000/wallet',
+        helperUrl: 'http://localhost:3000',
+      }
+    default:
+      throw Error(`Unconfigured environment '${env}'`)
+  }
+}
